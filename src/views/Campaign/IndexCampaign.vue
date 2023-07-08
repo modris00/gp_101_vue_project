ad553a98e5a2bab3073a5316091d6049e4e4825c
Sari Khaled <web.developer.sari@gmail.com>
2023-07-08 21:58:02 +0300

    edit , delete => indexCampaign

--- src/views/Campaign/IndexCampaign.vue
+++ src/views/Campaign/IndexCampaign.vue
@@ -1,15 +1,58 @@
 <template>
-  <TableComp
-    :headers="headers"
-    :rows="Campaigns"
-    :deleteItem="deleteRow"
-    title="Campaigns"
-  ></TableComp>
+  <div class="row">
+    <div class="col-12">
+      <div class="card">
+        <div class="card-header"><h3 class="card-title">All Campaigns</h3></div>
+        <!-- /.card-header -->
+        <div class="card-body">
+          <table id="Campaigns" class="table table-bordered table-striped">
+            <thead>
+              <th>#</th>
+              <th style="display: none">id</th>
+              <th>title</th>
+              <th>status</th>
+              <th>amount</th>
+              <th>start_date</th>
+              <th>end_date</th>
+              <th>admin_id</th>
+              <th>currency_name</th>
+              <th>Actions</th>
+            </thead>
+            <tbody>
+              <tr v-for="(item, index) in Campaigns" :key="index">
+                <td>{{ item.id }}</td>
+                <td>{{ item.title }}</td>
+                <td>{{ item.status }}</td>
+                <td>{{ item.amount }}</td>
+                <td>{{ item.start_date }}</td>
+                <td>{{ item.end_date }}</td>
+                <td>{{ item.admin_id }}</td>
+                <td>{{ item.currency_name }}</td>
+                <td>
+                  <router-link
+                    style="margin-right: 2px"
+                    class="btn btn-warning"
+                    :to="{ name: 'campaigns.edit', params: { id: item.id } }"
+                  >
+                    <i class="fas fa-edit"></i>
+                  </router-link>
+                  <!-- prettier-ignore --><button type="button" @click="deleteRow($event,item.id)" class="btn btn-danger"><i class="fas fa-trash-alt"></i></button>
+                </td>
+              </tr>
+              <!--v-if-->
+            </tbody>
+          </table>
+        </div>
+        <!-- /.card-body -->
+      </div>
+      <!-- /.card -->
+    </div>
+    <!-- /.col -->
+  </div>
 </template>
 
 <script>
 import axios from "axios";
-import TableComp from "../../components/Table.vue";
 
 export default {
   components: {
@@ -22,16 +65,6 @@ export default {
   data() {
     return {
       Campaigns: [],
-      headers: [
-        "id",
-        "title",
-        "status",
-        "amount",
-        "start_date",
-        "end_date",
-        "admin_id",
-        "currency_name",
-      ],
     };
   },
   methods: {
